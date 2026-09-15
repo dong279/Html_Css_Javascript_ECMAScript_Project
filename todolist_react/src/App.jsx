88b6fdd14f58864590624db067a3fe4b3d21249b
dong279 <dongho0130@daum.net>
2026-09-15 15:57:59 +0900

    MyComponentFunc add + app.jsx update

--- todolist_react/src/App.jsx
+++ todolist_react/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import MyComponent from "./components/MyComponent";
+import MyComponentFunc from "./components/MyComponentFunc";
 
 function App() {
   return (
     <>
-      <MyComponent name="홍길동" age={20} />
+      <MyComponent name="클래스형" age={20} />
+      <MyComponentFunc name="함수형" age={30} />
     </>
   );
 }
