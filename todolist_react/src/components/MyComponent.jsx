e20f8e972a1ff2e1d50bf04d2294955e740c9feb
dong279 <dongho0130@daum.net>
2026-09-15 15:55:12 +0900

    MyComponent update

--- todolist_react/src/components/MyComponent.jsx
+++ todolist_react/src/components/MyComponent.jsx
@@ -2,11 +2,12 @@ import React, { Component } from "react";
 
 class MyComponent extends Component {
   render() {
+    const { name, age } = this.props;
     return (
       <div>
         <h2>클래스 형태의 컴포넌트</h2>
         <h3>
-          Hello {this.props.name} / {this.props.age}
+          Hello {name} / {age}
         </h3>
       </div>
     );
