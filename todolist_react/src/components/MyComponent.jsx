acab14ba235b0aa851ce2ea09439402be19827d4
dong279 <dongho0130@daum.net>
2026-09-15 16:11:11 +0900

    update

--- todolist_react/src/components/MyComponent.jsx
+++ todolist_react/src/components/MyComponent.jsx
@@ -8,7 +8,7 @@ class MyComponent extends Component {
 
   render() {
     const { name, age } = this.props;
-    const { value } = this.value;
+    const { value } = this.state;
     return (
       <div>
         <h2>클래스 형태의 컴포넌트</h2>
